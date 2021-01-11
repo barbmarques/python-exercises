ac932879ca2a749dd886d47c6d13497c2314ed32
Barb Marques <barbara.ann.marques@gmail.com>
2021-01-11 15:16:35 -0600

    Commit Python Introduction Exercises

---
+++ python_introduction_exercises.py
@@ -0,0 +1,25 @@
+# Python 3.8.5
+
+print("Hello, World!")
+
+greeting = 'How are you doing today?'
+yesterday = 'snowing'
+today = 'Monday'
+
+print(greeting,'Today is',today,'.',' It was',yesterday,'yesterday!')
+
+
+# More notes from today:
+# REPL (read-evaluate-print-loop) is a feedback loop that works like a scratchpad
+# From terminal:  python -- type quit() to quit
+# From terminal: ipython -- type quit to quit
+# help() - accesses built in help system.  Type q to quit help
+
+# IN TERMINAL:  Command-N opens a new window; Command-T opens a new tab
+# TO RUN PYTHON FILE:  python file_name.py
+
+# JUPYTER - allows you to see immediate results.
+# To start the Jupyter Notebook server from terminal enter: jupyter notebook
+
+# INSTALLING PACKAGES - to install additional packages, use pip:
+#  pip install packagename
