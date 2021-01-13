d70d92d48d92f2e050a9e200ab2daa1094064346
Barb Marques <barbara.ann.marques@gmail.com>
2021-01-12 18:49:11 -0600

    Data Types and Variables Exercises -- corrected

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -9,11 +9,15 @@
 # going to like it). If price for a movie per day is 3 dollars, how much will you have 
 # to pay?
 
-little_mermaid = 3
-brother_bear = 5
-hercules = 1
+little_mermaid_days = 3  #Variables hold nouns
+brother_bear_days = 5
+hercules_days = 1
+price_per_movie = 3
+
+
 print(3 * (little_mermaid + brother_bear + hercules))
 
+-- 27
 
 # Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, 
 # they pay you a different rate per hour. Google pays 400 dollars per hour, Amazon 380,
@@ -26,25 +30,54 @@ facebook_rate = 350
 google_hours = 6
 amazon_hours = 4
 facebook_hours = 10
---  print((6*google)+(10*facebook)+(4*amazon))
--- use dictionary for each company, with rate, hours)
+amazon_subtotal = amazon_hours * amazon_rate
+google_subtotal = google_hours * google_rate
+facebook_subtotal = facebook_hours * facebook_rate
+total = amazon_subtotal + facebook_subtotal + google_subtotal
+
+f"${total} is the total of consulting with these companies"
+
+-- '$7420 is the total of consulting with these companies'
+
 
 # A student can be enrolled to a class only if the class is not full and the class schedule
 #  does not conflict with her current schedule.
 
--- student_name:
--- class_1: 
--- class_2:
--- class_3:
--- class_4:
+class_has_space = True
+schedule_works = True
+can_be_enrolled = class_has_space and schedule_works
+can_be_enrolled 
 
-current_enroll: current number of students enrolled 
-max_enroll: maximum allowable enrollment
-
-
+--  True
 # A product offer can be applied only if people buys more than 2 items, and the offer has not
 #  expired. Premium members do not need to buy a specific amount of products.
 
-total_items: total items purchased
-is_expired: bool
-is_premium: bool
+has_not_expired = True
+buys_two_or_more = True
+is_premium_member = True
+offer_available = has_not_expired and (buys_two_or_more or is_premium_member)
+offer_available
+
+-- True
+
+
+# Use the following code to follow the instructions below:
+
+# username = 'codeup'
+# password = 'notastrongpassword'
+# Create a variable that holds a boolean value for each of the following conditions:
+
+-- the password must be at least 5 characters
+-- the username must be no more than 20 characters
+-- the password must not be the same as the username
+-- bonus neither the username or password can start or end with whitespace
+#~~~~~~~~~~~~~~~~~~~~
+if username[0] != ' ' and username[-1] != ' '
+and password[0] != ' ' and password[0] != ' '
+#~~~~~~~~~~~~~~~~~~~
+
+pw_is_at_least_five_characters = len(password) >= 5
+username_less_than_twenty  == len(username) <= 20
+password_differs_from_username = username != password
+password_no_begin_or_end_whitespace =  password == password.strip()
+username_no_begin_or_end_whitespace =  username == username.strip()
